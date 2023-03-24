0e52089070813bf8f7736586cad0d506055fc1a6
MoyBadillo117 <A00834306@tec.mx>
2023-03-23 18:12:50 -0600

    Se agregó que el juego fuera infinito

--- cannon.py
+++ cannon.py
@@ -66,8 +66,10 @@ def move():
     draw()
 
     for target in targets:
-        if not inside(target):
-            return
+        if target.x<-200:
+            target.x=200
+            target.y=randrange(-150,150)
+            
 
     ontimer(move, 5)
 
